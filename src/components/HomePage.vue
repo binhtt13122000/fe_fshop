75856557b05c01c8105cd6dde543179428a7b0e1
nhanlt1602 <nhanlt16235@gmail.com>
2021-01-02 09:04:34 +0700

    [NhanLT] create navigation bar right commits

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,12 +2,12 @@
   <div id="app">
     <v-app id="inspire">
       <v-app id="inspire">
-    <!-- Navigation bar -->
+        <!-- Navigation bar -->
         <v-navigation-drawer
-            v-model="drawer"
-            :clipped="$vuetify.breakpoint.lgAndUp"
-            app
-            right
+          v-model="drawer"
+          :clipped="$vuetify.breakpoint.lgAndUp"
+          app
+          right
         >
           <v-text-field
             flat
@@ -17,55 +17,83 @@
             label="Search"
             class="nav-bar-search"
           ></v-text-field>
-          <v-list dense>
-
+          <v-list class="nar-bar-btn"  dense  justify="center" align="center">
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title>Name</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title>Nữ</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title>Bộ sưu tập</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title>Blog</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title>Khuyến mãi</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title>Hệ Thống cửa hàng</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
           </v-list>
         </v-navigation-drawer>
-    <!--App bar -->
+        <!--App bar -->
         <v-app-bar
           :clipped-right="$vuetify.breakpoint.lgAndUp"
           app
           color="grey darken-3"
           dark
-          style="height: 80px;"
+          style="height: 65px"
           class="app-bar"
         >
-            <div class="site-logo">
-                <a href="#" to="/">
-                    <img
-                        class="img-responsive"
-                        src="https://4menshop.com/logo.png"
-                        alt="4men"
-                    />
-                </a>
-            </div>
-            <v-row width="90px" justify="center">
-                <v-btn v-for="link in linkBar" :key="link" color="white" text>{{
-                link
-                }}</v-btn>
-                
-            </v-row>
-    
-    <!-- Create right column -->
+          <div class="site-logo">
+            <a href="#" to="/">
+              <img
+                class="img-responsive"
+                src="https://4menshop.com/logo.png"
+                alt="4men"
+              />
+            </a>
+          </div>
+          <v-row width="90px" justify="center">
+            <v-btn v-for="link in linkBar" :key="link" color="white" text>{{
+              link
+            }}</v-btn>
+          </v-row>
 
-            <v-spacer></v-spacer>
-            <v-text-field
-                flat
-                solo-inverted
-                hide-details
-                prepend-inner-icon="mdi-magnify"
-                label="Search"
-                class=hidden-md-and-down
-            ></v-text-field>
-            <div class="right-column-icon">
-                <v-btn icon class="hidden-xs-and-down">
-                    <v-icon>mdi-shopping</v-icon>
-                </v-btn>
-                <v-btn icon to="/loginpage" class="hidden-xs-and-down">
-                    <v-icon>mdi-account</v-icon>
-                </v-btn>
-            </div>
-            <v-app-bar-nav-icon
+          <!-- Create right column -->
+
+          <v-spacer></v-spacer>
+          <v-text-field
+            flat
+            solo-inverted
+            hide-details
+            prepend-inner-icon="mdi-magnify"
+            label="Search"
+            class="hidden-md-and-down"
+          ></v-text-field>
+          <div class="right-column-icon">
+            <v-btn icon class="hidden-xs-and-down">
+              <v-icon>mdi-shopping</v-icon>
+            </v-btn>
+            <v-btn icon to="/loginpage" class="hidden-xs-and-down">
+              <v-icon>mdi-account</v-icon>
+            </v-btn>
+          </div>
+          <v-app-bar-nav-icon
             @click.stop="drawer = !drawer"
             v-if="!isValid"
           ></v-app-bar-nav-icon>
@@ -75,11 +103,11 @@
           <v-container fluid>
             <v-row align="center" justify="center">
               <div class="main-image">
-                <img :src="mainImageSrc" />
+                <img class="img-responsive" :src="mainImageSrc" />
               </div>
               <div class="image-list">
                 <div v-for="img in images" :key="img" class="item">
-                  <img src="img.image" />
+                  <img class="img-responsive" src="img.image" />
                 </div>
               </div>
             </v-row>
@@ -96,7 +124,7 @@
               rounded
               class="my-2"
             >
-              {{ link }}    
+              {{ link }}
             </v-btn>
             <v-col class="grey darken-1 py-4 text-center white--text" cols="12">
               {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
@@ -130,7 +158,7 @@ export default {
   }),
   methods: {},
   created() {
-    let self = this
+    let self = this;
     this.images = [
       {
         id: "1",
@@ -158,15 +186,15 @@ export default {
 
 <style lang="scss">
 .app-bar {
-    opacity: 15%;
+  opacity: 15%;
 }
 
 .app-bar:hover {
-    opacity: 100%;
+  opacity: 100%;
 }
 .bar-center {
-    background-color: #ffffff;
-    opacity: 60%;
+  background-color: #ffffff;
+  opacity: 60%;
 }
 
 @media only screen and (max-width: 850px) {
