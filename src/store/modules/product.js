f1f639469502dae87f21ab3716f6b20ec6147a01
BinhTT1312 <binhtt.13122000@gmail.com>
2021-02-07 15:04:02 +0700

    add mutation type

--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,7 +1,5 @@
-// import { reject, resolve } from "core-js/fn/promise";
-// import { reject } from 'core-js/fn/promise';
-import 'es6-promise/auto'
 import ProductServices from "../../services/ProductService"
+import { SET_PAGE, SET_PAGES, SET_PRODUCT, SET_PRODUCTS } from './product/mutation-type'
 
 const state = {
     products: [],
@@ -28,49 +26,60 @@ const getters = {
 
 
 const mutations = {
-    setProducts: (state, val) => {
+    [SET_PRODUCTS]: (state, val) => {
         state.products = val
     },
-    setProduct: (state, val) => {
+    [SET_PRODUCT]: (state, val) => {
         state.product = val
     },
-    setPages: (state, val) => {
+    [SET_PAGES]: (state, val) => {
         state.pages = val
     },
 
-    setpage: (state, val) => {
+    [SET_PAGE]: (state, val) => {
         state.page = val
     }
-    
+
 };
 
 const actions = {
 
-    getProducts({ commit }, index) {
-        return new Promise((resolve, reject) => {
-            ProductServices.getProducts(index).then((resp) => {
-                commit("setProducts", resp.data.content)
-                commit("setPages", resp.data)
-                resolve(resp)
-            })
-                .catch(err => {
-                    console.log(err)
-                    reject(err)
-                })
-        })
+    async getProducts({ commit }, index) {
+            const response = await ProductServices.getProducts(index);
+            if (response.status === 200) {
+                await commit(SET_PRODUCTS, response.data.content);
+                return await commit(SET_PAGES, response.data)
+            }
+            throw new Error(response.status);
+        // return new Promise((resolve, reject) => {
+        //     ProductServices.getProducts(index).then((resp) => {
+        //         commit("setProducts", resp.data.content)
+        //         commit("setPages", resp.data)
+        //         resolve(resp)
+        //     })
+        //         .catch(err => {
+        //             console.log(err)
+        //             reject(err)
+        //         })
+        // })
     },
 
-    productDetails({ commit }, id) {
-        return new Promise((resolve, reject) => {
-            ProductServices.getProductsByProductId(id).then((response) => {
-                commit("setProduct", response.data)
-                resolve(response)
-            })
-                .catch(err => {
-                    console.log(err)
-                    reject(err)
-                })
-        })
+    async productDetails({ commit }, id) {
+        const response = await ProductServices.getProductsByProductId(id);
+        if(response.status === 200){
+            return commit(SET_PRODUCT, response.data);
+        }
+        throw new Error(response.status);
+        // return new Promise((resolve, reject) => {
+        //     ProductServices.getProductsByProductId(id).then((response) => {
+        //         commit("setProduct", response.data)
+        //         resolve(response)
+        //     })
+        //         .catch(err => {
+        //             console.log(err)
+        //             reject(err)
+        //         })
+        // })
     },
 };
 
@@ -78,6 +87,6 @@ export default {
     namespaced: true, // giup nhan biet dispatch, phan biet den store nao
     state,
     getters,
-    mutations, 
+    mutations,
     actions
 }
---
+++ src/store/modules/product/mutation-type.js
@@ -0,0 +1,4 @@
+export const SET_PRODUCTS = "setProducts";
+export const SET_PRODUCT = "setProduct";
+export const SET_PAGES = "setPages";
+export const SET_PAGE = "setPage";
